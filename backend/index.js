dcb6ec86e125ad959546258d1e3fceb2fbfb8a15
Khushboo Raikwar <j.khushbooraikwar95@gmail.com>
2021-11-26 08:39:44 +0530

    Data sent to mongo using POST

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,11 @@ connectToMongo();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Khush!')
-})
+app.use(express.json());
+
+//Avaiable routes
+app.use('/api/auth', require('./routes/auth'))
+//app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
