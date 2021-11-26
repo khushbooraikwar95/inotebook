45081e41bae9754fc31b9ec8b90d0ad3f110dba5
Khushboo Raikwar <j.khushbooraikwar95@gmail.com>
2021-11-26 09:28:31 +0530

    Added data validations using express validator

--- backend/models/User.js
+++ backend/models/User.js
@@ -21,4 +21,6 @@ const UserSchema = new Schema({
     default: Date.now,
   },
 });
-module.exports = mongoose.model("user", UserSchema);
+const User = mongoose.model("user", UserSchema);
+User.createIndexes();
+module.exports = User;
